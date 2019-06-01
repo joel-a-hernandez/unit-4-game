97bc92808b665718d04c6a1920850920252d86f0
Joel Hernandez <joel24hernan@gmail.com>
2019-05-31 21:33:29 -0500

    Got rid of a lot of the html because i will dynamically create it with jquerry. Added for loop to generate random number for the crystals.

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+var wins;
+var losses;
+var randomStarterNum;
+
+
+for (var i = 0; i < 4; i++){
+    var random = Math.floor(Math.random() * 12);
+    console.log(random);
+
+
+}
+
+//wins losses & total score need to start at 0 
+//random number needs to generate from 19-120
+//when user clicks one of the 4 crystals, a random number from 1 to 12 needs to generate for that crystal
+//
